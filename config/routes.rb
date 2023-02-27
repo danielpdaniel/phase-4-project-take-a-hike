1bca16adf10d00dc59b442a011dd3f1fdd6d64e2
danielpdaniel <artofjquinn@gmail.com>
2023-02-27 17:43:39 -0500

    set up basic /login post route for creating session on login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
+  post "/login", to: "sessions#create"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
