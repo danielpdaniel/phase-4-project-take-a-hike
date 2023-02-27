609425f0bc4ce051d847675ecf0c64b6b7015dd1
danielpdaniel <artofjquinn@gmail.com>
2023-02-27 17:04:02 -0500

    add basic form with two input fields and submit button to Login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Login() {
+    
+
     return (
-        <h2>Wahoo! Time to login!</h2>
+        <div>
+            <h2>Wahoo! Time to login!</h2>
+            <form>
+                <label>Username:</label>
+                    <input type="text" name="username"/>
+                <label>Password:</label>
+                    <input type="text" name="password"/>
+                <input type="submit" name="login_btn"/>
+            </form>
+        </div>
     )
 }
 
