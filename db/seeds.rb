50b5b619f976935b64486335d2fdc7fa54bed426
danielpdaniel <artofjquinn@gmail.com>
2023-02-27 16:35:20 -0500

    create a test user through the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create({username: "hotdoggity", password: "cornchip123", avatar_image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0aEU6jSQwAlQsEWf2LroMQHaHa%26pid%3DApi&f=1&ipt=99c6b96478b3874ee9c3b059a43d2177a1482a319a7c9cbd26e87dcaf63ed594&ipo=images", about: "its me >:3"})
