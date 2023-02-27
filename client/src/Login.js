6c68bba6d03bf13a1536c9e99be995c69e7e019d
danielpdaniel <artofjquinn@gmail.com>
2023-02-27 17:56:02 -0500

    add state controlled username and password values to frontend login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react";
 
 function Login() {
-    
+    const [usernameInput, setUsernameInput] = useState("")
+    const [passwordInput, setPasswordInput] = useState("")
+
+    function handleLoginSubmit(e){
+        e.preventDefault()
+        console.log(usernameInput, passwordInput)
+    }
 
     return (
         <div>
             <h2>Wahoo! Time to login!</h2>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
                 <label>Username:</label>
-                    <input type="text" name="username"/>
+                    <input type="text" name="username" value={usernameInput} onChange={(e)=>setUsernameInput(e.target.value)} placeholder="type here..."/>
                 <label>Password:</label>
-                    <input type="text" name="password"/>
+                    <input type="text" name="password" value={passwordInput} onChange={(e)=>setPasswordInput(e.target.value)} placeholder="type here..."/>
                 <input type="submit" name="login_btn"/>
             </form>
         </div>
