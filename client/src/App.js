32cccfb8aabce58781628fddbea5cf5e0a61ff6a
danielpdaniel <artofjquinn@gmail.com>
2023-02-28 16:14:22 -0500

    add basic Home component + route / path to it

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {Route, Routes} from 'react-router-dom'
 import React, {useState, useEffect} from 'react'
 import { UserProvider } from "./context/user"
 import NavBar from './NavBar';
+import Home from './Home';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <UserProvider>
         <NavBar />
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login />}/>
       </Routes>
       </UserProvider>
