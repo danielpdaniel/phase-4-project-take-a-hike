c13ebec80fd7ec05ca991cfb30f2e2f285e96522
danielpdaniel <artofjquinn@gmail.com>
2023-03-01 16:28:04 -0500

    update styling with a little logo sketch

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,11 +1,13 @@
 import react, { useContext} from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "./context/user";
+import logo from "./take_a_hike_logo.png"
 
 function NavBar(){
     const { user } = useContext(UserContext)
     return(
         <nav className="navbar">
+            <NavLink to="/"><img src={logo} id="logo"/></NavLink>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/login">{user ? "AH" : "Login"}</NavLink>
         </nav>
