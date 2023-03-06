6cb333374e468a671bfd2af5c863831168805ee8
danielpdaniel <artofjquinn@gmail.com>
2023-03-06 16:04:35 -0500

    add new trail form to /trails frontend page

--- client/src/Trails.js
+++ client/src/Trails.js
@@ -12,6 +12,20 @@ function Trails(){
             <li>
                 {trails ? trails.map(trail=><ul key={trail.id}>{trail.name}</ul>): <ul>Loading...</ul>}
             </li>
+            <form className="new_trail_form">
+                <label>Name: </label>
+                    <input type="text"/>
+                <label>Location: </label>
+                    <input type="text"/>
+                <label>Description: </label>
+                    <textarea />
+                <label>Image link: </label>
+                    <input type="text"/>
+                <label>Distance (miles): </label>
+                    <input type="text"/>
+                <label>Intensity (out of 10): </label>
+                    <input type="text"/>
+            </form>
         </div>
     )
 }
