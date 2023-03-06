32bcf23abc036d10779944a70d5694afe7996f90
danielpdaniel <artofjquinn@gmail.com>
2023-03-06 18:06:29 -0500

    add validation to ensure trail distance value is a number

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -3,6 +3,6 @@ class Trail < ApplicationRecord
     validates :location, presence: true
     validates :description, presence: true, length: {minimum: 5, maximum: 1000}
     validates :image, presence: true
-    validates :distance, presence: true
+    validates :distance, presence: true, numericality: true
     validates :intensity, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10}
 end
