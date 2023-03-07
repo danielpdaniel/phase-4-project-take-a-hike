76576b071f19ae06bc2282e650c813e3b344ddac
danielpdaniel <artofjquinn@gmail.com>
2023-03-06 19:46:42 -0500

    use useNavigate hook to redirect to home page on succesful login

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,13 @@
 import {useState, useContext} from "react";
 import { UserContext } from "./context/user";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [errors, setErrors] = useState("")
+
+    const navigate = useNavigate()
    
 
     const { user, setUser } = useContext(UserContext)
@@ -25,6 +28,9 @@ function Login() {
         .then(r=>{
             if(r.ok){
                 r.json().then(setUser)
+                setUsername("")
+                setPassword("")
+                navigate("/")
             } else {
                 r.json().then(e => setErrors(e.error))
             }
@@ -67,13 +73,19 @@ function Login() {
         <div>
             {!user? <h2>Wahoo! Time to login!</h2>: <h2>Wahoo! You're logged in!</h2>}
             <form onSubmit={handleLoginSubmit}>
-                <label>Username:</label>
+                <label>Username: </label>
                     <input type="text" name="username" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder="type here..."/>
-                <label>Password:</label>
+                <label>Password: </label>
                     <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="type here..."/>
-                <input type="submit" name="login_btn"/>
-                <input type="button" name="logout_btn" onClick={handleLogout} value="Logout"/>
+                {!user? 
+                <div>
+                <input type="submit" name="login_btn" value="Login"/>
                 <input type="button" name="signup_btn" value="Signup" onClick={handleSignup}/>
+                </div>
+                :
+                <div>
+                <button onClick={handleLogout}>Logout</button>
+                </div>}
             </form>
             {errors ? <div><h4>Error!</h4>{Object.keys(errors).map(key => <h4>{key}: {errors[key]}</h4>)}</div> : null}
         </div>
