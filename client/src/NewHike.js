4f0c5c30536e30441ec6942587ed6178aee41326
danielpdaniel <artofjquinn@gmail.com>
2023-03-08 16:15:52 -0500

    setup state values for new hike form + effect to fetch trails for rendering trail options in a select dropdown

--- client/src/NewHike.js
+++ client/src/NewHike.js
@@ -1,10 +1,34 @@
+import { useState, useContext, useEffect } from "react";
+import { UserContext } from "./context/user";
 
 function NewHike(){
+
+    const { user } = useContext(UserContext)
+    const [trails, setTrails] = useState("")
+    const [trailId, setTrailId] = useState("")
+    const [rating, setRating] = useState("")
+    const [notes, setNotes] = useState("")
+    const [image, setImage] = useState("")
+    const [date, setDate] = useState("")
+
+    useEffect(()=>{
+        fetch("/trails")
+        .then(r=>r.json())
+        .then(data => setTrails(data))
+    }, [])
+
+    function handleHikePostSubmit(e){
+        e.preventDefault()
+        console.log(user)
+
+    }
     return (
         <div>
             <h3>Add New Hike!</h3>
-            <form className="newHikeForm">
-                <input></input>
+            <form className="newHikeForm" onSubmit={(e)=>handleHikePostSubmit(e)}>
+                <select>
+                    {trails ? trails.map(trail => <option>{trail.name}</option>) : <option>loading...</option>}
+                </select>
             </form>
         </div>
     )
