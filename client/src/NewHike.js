2f6f7c3950346e824bc1ed1f1b8be3911e0f8b4c
danielpdaniel <artofjquinn@gmail.com>
2023-03-08 17:11:56 -0500

    add date input field to new hike form

--- client/src/NewHike.js
+++ client/src/NewHike.js
@@ -25,6 +25,7 @@ function NewHike(){
             rating: rating,
             notes: notes,
             image: image,
+            date: date
 
         }
         console.log(postBody)
@@ -40,6 +41,7 @@ function NewHike(){
         setRating(e.target.id)
     }
 
+
     return (
         <div>
             <h3>Add New Hike!</h3>
@@ -51,17 +53,20 @@ function NewHike(){
                     </select>
 
                 <label>Rating:</label>
-                    <input id={1} className={rating >= 1 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 1 ? true : false} onClick={(e)=>handleRatingClick(e)} readOnly={true}/>
-                    <input id={2} className={rating >= 2 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 2 ? true : false} onClick={(e)=>handleRatingClick(e)} readOnly={true}/>
-                    <input id={3} className={rating >= 3 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 3 ? true : false} onClick={(e)=>handleRatingClick(e)} readOnly={true}/>
-                    <input id={4} className={rating >= 4 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 4 ? true : false} onClick={(e)=>handleRatingClick(e)} readOnly={true}/>
-                    <input id={5} className={rating >= 5 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 5 ? true : false} onClick={(e)=>handleRatingClick(e)} readOnly={true}/>
+                    <input id={1} className={rating >= 1 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 1 ? true : false} onClick={handleRatingClick} readOnly={true}/>
+                    <input id={2} className={rating >= 2 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 2 ? true : false} onClick={handleRatingClick} readOnly={true}/>
+                    <input id={3} className={rating >= 3 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 3 ? true : false} onClick={handleRatingClick} readOnly={true}/>
+                    <input id={4} className={rating >= 4 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 4 ? true : false} onClick={handleRatingClick} readOnly={true}/>
+                    <input id={5} className={rating >= 5 ? "clickedRating" : "unClickedRating"} type="radio" checked={rating >= 5 ? true : false} onClick={handleRatingClick} readOnly={true}/>
 
                 <label>Notes:</label>
                     <textarea onChange={(e)=>setNotes(e.target.value)}/>
 
                 <label>Image:</label>
                     <input type="text" onChange={(e)=>setImage(e.target.value)}/>
+                
+                <label>Date:</label>
+                    <input type="date" onChange={(e)=>setDate(e.target.value)}/>
 
                     <input type="submit"/>
 
