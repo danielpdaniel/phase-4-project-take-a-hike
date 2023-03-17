64a0108fe0f6b1afa3b5ac0fbb2d50068cbb9b4e
danielpdaniel <artofjquinn@gmail.com>
2023-03-17 10:59:30 -0400

    remove/comment out /sessions get request

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # post "/login", to: "sessions#create"
   # delete "/logout", to: "sessions#destroy"
 
-  resources :sessions, only: [:create, :destroy, :show]
+  resources :sessions, only: [:create, :destroy]
 
   resources :trails, only: [:index, :create, :show]
 
