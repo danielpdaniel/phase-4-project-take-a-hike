b1c7131ef1e6bdccd27ea3da1740f7648c0ea07c
danielpdaniel <artofjquinn@gmail.com>
2023-03-19 21:30:11 -0400

    define name method in hike serializer for sending trail name with the rest of the hike object

--- app/serializers/hike_serializer.rb
+++ app/serializers/hike_serializer.rb
@@ -1,4 +1,8 @@
 class HikeSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :trail_id, :rating, :notes, :image, :date
+  attributes :id, :user_id, :trail_id, :rating, :notes, :image, :date, :name
+  
+  def name
+    Trail.find_by(id: self.object.trail_id).name
+  end
 
 end
