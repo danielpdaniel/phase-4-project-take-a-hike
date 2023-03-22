a27844ae3d6593d88e2a6da0f0594e62cccf8814
danielpdaniel <artofjquinn@gmail.com>
2023-03-22 09:56:05 -0400

    update User model to send nested trails in alphabetical order by name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :hikes, -> {order(date: :DESC)}
-    has_many :trails, through: :hikes
+    has_many :trails, -> {order(name: :ASC)}, through: :hikes
 
     has_secure_password
     validates :username, presence: true, uniqueness: true
